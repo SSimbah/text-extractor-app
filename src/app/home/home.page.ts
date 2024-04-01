e01cf7b1319e9c4ce0af0fbd3ca2a0a666285df4
Kyle Fernandez <fkyleowie@gmail.com>
2024-04-01 14:20:29 +0800

    Initial Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-
+import { Component, NgModule } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonIcon, IonGrid, IonRow, IonCol, IonImg } from '@ionic/angular/standalone';
+import { PhotoService } from '../services/photo.service';
+import { addIcons } from 'ionicons'
+import { camera } from 'ionicons/icons'
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonIcon, IonGrid, IonRow, IonCol, IonImg],
 })
 export class HomePage {
-  constructor() {}
+  constructor(public photoService: PhotoService) {
+    addIcons({camera});
+   }
+
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
 }
